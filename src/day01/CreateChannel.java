ba5fd491a0ed893359fa61a09894d098b9893e77
IrinaKV <61922863+IrinaKV@users.noreply.github.com>
2020-05-08 15:43:04 -0400

    Update CreateChannel.java

--- src/day01/CreateChannel.java
+++ src/day01/CreateChannel.java
@@ -3,5 +3,6 @@ package day01;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("New Project");
+        System.out.println("Second text");
     }
 }
